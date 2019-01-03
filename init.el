24313a151c376c0de0fa1fe3b37cd834aaada8bf
nakazye <nakazye@gmail.com>
2019-01-03 20:41:28 +0900

    add package manager (straight.el)

--- init.el
+++ init.el
@@ -85,6 +85,22 @@
       '(("." . "~/.emacs.d/var/backup/")))
 (setq auto-save-list-file-prefix "~/.emacs.d/var/auto-save-list/.saves-")
 
+;===================================
+; package manager: straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
 ;########################################
 ; package settings
 ;########################################
