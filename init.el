3ea4ea5bb9b42cc6846c6cfb60c27ed9d87952ba
Nakajima <yutaka.nakajima@accenture.com>
2019-12-10 17:12:35 +0900

    change modeline icon

--- init.el
+++ init.el
@@ -83,7 +83,7 @@
       (column-number-mode 0)
       (doom-modeline-def-modeline 'main
     '(bar  window-number matches buffer-info remote-host buffer-position parrot selection-info)
-    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
+    '(misc-info persp-name lsp github minor-modes input-method major-mode process vcs checker)))
 
 ;===================================
 ; font setting
